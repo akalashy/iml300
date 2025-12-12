fddae0b79b28fe5eac606e197ca2037d5116d55e
akalashy <akalashy@usc.edu>
2025-12-11 20:34:37 -0800

    fixed the exercises

---
+++ exercises/exercise4.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 300);
+}
+
+function draw() {
+  background(80, 20, 20);
+  fill(255);
+  textAlign(CENTER, CENTER);
+  textSize(24);
+  text("Exercise 4", width / 2, height / 2);
+}
